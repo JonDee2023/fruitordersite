268c4d580d215745136816f4071383ec57ca29ae
JOHN DADA <123139569+JonDee2023@users.noreply.github.com>
2026-03-29 17:38:41 +0100

    updated homepage

--- fruitplace1.js
+++ fruitplace1.js
@@ -1,2 +1,29 @@
 let hd = document.getElementById("biz-name");
 hd.innerText = "FarmHouse.ng";
+
+//const menuBtn = document.getElementById("menuBtn");
+//const navBar = document.getElementById("navBar");
+
+//menuBtn.addEventListener("click", function () {
+//  if (navBar.style.display === "block") {
+//    navBar.style.display = "none";
+//  } else {
+//    navBar.style.display = "block";
+//  }
+//}); 
+
+const menuBtn = document.getElementById("menuBtn");
+const navBar = document.getElementById("navBar");
+
+/* Toggle menu */
+menuBtn.addEventListener("click", function (e) {
+  e.stopPropagation(); // prevent triggering document click
+  navBar.classList.toggle("show");
+});
+
+/* Close when clicking outside */
+document.addEventListener("click", function (e) {
+  if (!navBar.contains(e.target) && !menuBtn.contains(e.target)) {
+    navBar.classList.remove("show");
+  }
+});
