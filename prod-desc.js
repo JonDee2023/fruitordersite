2b945e7c89d585b1e47d2928164b843a90893945
JOHN DADA <123139569+JonDee2023@users.noreply.github.com>
2026-04-09 10:33:09 +0100

    Update prod-desc.js

--- prod-desc.js
+++ prod-desc.js
@@ -36,31 +36,31 @@ document.addEventListener(DOMContentLoaded, ()=>{
      price: "#5,000"
     }
 
-]
+    ]
 
 
-let index = 0;
+    let index = 0;
 
-function updateProduct(){
-    
-    document.getElementById("prod-img").src = prodDesc[index].img;
-    document.getElementById("prod-desc").textContent = prodDesc[index].desc;
-    document.getElementById("prod-price").textContent = prodDesc[index].price;
+    function updateProduct(){
+        
+        document.getElementById("prod-img").src = prodDesc[index].img;
+        document.getElementById("prod-desc").textContent = prodDesc[index].desc;
+        document.getElementById("prod-price").textContent = prodDesc[index].price;
 
 
-}
+    }
 
-// next button
-document.getElementById("nextBtn").addEventListener("click", ()=>{
-    index = (index+1) % prodDesc.length;
-    updateProduct();
-});
+    // next button
+    document.getElementById("nextBtn").addEventListener("click", ()=>{
+        index = (index+1) % prodDesc.length;
+        updateProduct();
+    });
 
-// Previous button
-document.getElementById("prevBtn").addEventListener("click", () => {
-    index = (index - 1 + prodDesc.length) % prodDesc.length;
-    updateProduct();
-});
+    // Previous button
+    document.getElementById("prevBtn").addEventListener("click", () => {
+        index = (index - 1 + prodDesc.length) % prodDesc.length;
+        updateProduct();
+    });
 
 
 });
